f3e78ac5929bbf377ea18138c7d48c860de22afa
aayushi kambariya <aayushi.kambriya5@gmail.com>
2020-05-31 20:44:15 +0530

    Updates

---
+++ precache-manifest.a3048604ed026bc920755cda0c6db1f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0377494d4b5732053a51971d888946",
+    "url": "/React2020-monsterproject/index.html"
+  },
+  {
+    "revision": "5e4b1c312d705d5e7466",
+    "url": "/React2020-monsterproject/static/css/main.4ca08b25.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/React2020-monsterproject/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React2020-monsterproject/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4b1c312d705d5e7466",
+    "url": "/React2020-monsterproject/static/js/main.3fb2c164.chunk.js"
+  },
+  {
+    "revision": "b448641e02929f415d9f",
+    "url": "/React2020-monsterproject/static/js/runtime-main.825cb310.js"
+  }
+]);
